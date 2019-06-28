2515eca99582dcb2f0350234ad5de4689f6c4d46
johnmvergara <johnmichael.vergara.iics@ust.edu.ph>
2019-06-28 13:57:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/quiz2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.quiz2;
+
+import android.app.Activity;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showMessage(View v) {
+        Toast toast = Toast.makeText(this, "Cancel button was clicked...", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    public void showMessage2(View c){
+        Snackbar sbar = Snackbar.make(c, "Pinindot si buton...", Snackbar.LENGTH_LONG);
+        sbar.show();
+    }
+
+}
+
+
